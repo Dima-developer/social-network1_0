83e4570b7293e1da4390d3e32e52a3510f3b3b77
Dmitrij (Dima) Petrenko <64629013+Dima-developer@users.noreply.github.com>
2020-07-14 22:05:20 +0300

    Add folders via upload

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://www.krym4you.com/files/catalog/138/gallery/big//chernoe-more_1435752144.jpg' />
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Profile;
