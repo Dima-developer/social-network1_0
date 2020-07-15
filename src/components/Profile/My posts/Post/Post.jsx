23b251e60c5974b22f5f904acb788170c59c97bb
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-07-15 12:33:09 +0300

    U.16

--- src/components/Profile/My posts/Post/Post.jsx
+++ src/components/Profile/My posts/Post/Post.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
+
+
     return (
         <div className={classes.item}>
             <img src="https://whatsism.com/uploads/posts/2018-07/1530546770_rmk_vdjbx10.jpg" alt="ava" />
-                post 1
+            { props.message }
             <div>
                 <span>Like</span>
             </div>
