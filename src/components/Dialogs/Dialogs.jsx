ea35887069ea36d9c0bb4db4fe173c3377c70ff9
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-07-17 14:07:58 +0300

    U28

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,24 +7,11 @@ import Message from './Message/Message';
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        { id: 1, name: "Dmytro" },
-        { id: 2, name: "Julia" },
-        { id: 3, name: "Arina" },
-        { id: 4, name: "Alex" },
-        { id: 5, name: "Mark" },
-        { id: 6, name: "Masha" }
-    ];
 
-    let messages = [
-        { id: 1, message: 'Hi' },
-        { id: 2, message: 'How is your it-kamasutra?' },
-        { id: 3, message: 'Yo' }
-    ];
 
-    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
 
-    let messagesElements = messages.map(m => <Message message={m.message} />);
+    let messagesElements = props.messages.map(m => <Message message={m.message} />);
 
     return (
         <div className={classes.dialogs}>
--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -5,12 +5,12 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
-    let posts = [
-        { id: 1, message: 'Hi, how are you?', likesCount: 4 },
-        { id: 2, message: "It's my first post", likesCount: 23 },
-    ];
+    // let posts = [
+    //     { id: 1, message: 'Hi, how are you?', likesCount: 4 },
+    //     { id: 2, message: "It's my first post", likesCount: 23 },
+    // ];
 
-    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount} />);
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />);
 
     return (
         <div className={classes.postsBlock}>
