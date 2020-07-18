71ad7b6cd117aa7deb77299607df815573dad589
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-07-19 00:16:21 +0300

    U32

--- src/components/Navbar/Friends/Friend/Friend.jsx
+++ src/components/Navbar/Friends/Friend/Friend.jsx
@@ -7,7 +7,7 @@ const Friend = (props) => {
     return (
         <div className={classes.friends}>
             <div className={classes.friendItem}>
-                <img src="https://lh3.googleusercontent.com/proxy/rlNlkWAu962KjEUzm-BmnJPPYbEr6dvj2_tu27YMmQT383Xe6IYelDfyFERxqHPi7ZUWnU9LVhQIi6Ok2dwsdxovqO8n1ifrYzGQWPsa5grTohNmaI1HkPw48z9s3M-6Ik_ARyNvGScgZ6Cxe8xi6NwiEr1QQa_ch_H4JIgnn4K6WzMbFljDzxHInj4Wubw" alt="ava" />
+                <img src="./images/Lion.jpg" alt="ava" />
                 {props.name}
             </div>
         </div>
