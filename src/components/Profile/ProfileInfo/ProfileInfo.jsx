a8e13ac54aee6916cfe43c2e71991783d005b4af
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-07-30 16:01:16 +0300

    U60

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import classes from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader />
+    }
+
     return (
         <div>
             <div className={classes.photo}>
                 <img src='https://www.krym4you.com/files/catalog/138/gallery/big//chernoe-more_1435752144.jpg' />
             </div>
             <div className={classes.descriptionBlock}>
+                <img src={props.profile.photos.large} />
                 ava + description
             </div>
 
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,12 +1,14 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 let initialState = {
     posts: [
         { id: 1, message: 'Hi, how are you?', likesCount: 4 },
         { id: 2, message: "It's my first post", likesCount: 23 }
     ],
-    newPostText: 'IT-kamasutra!'
+    newPostText: 'IT-kamasutra!',
+    profile: null
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -30,12 +32,16 @@ const profileReducer = (state = initialState, action) => {
             };
             //stateCopy.newMessageText = action.newMessage;
         }
+        case SET_USER_PROFILE: {
+            return{ ...state, profile: action.profile}
+        }
         default:
             return state;
     }
 }
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
 export const updateNewPostTextActionCreator = (text) =>
     ({ type: UPDATE_NEW_POST_TEXT, newText: text });
 
