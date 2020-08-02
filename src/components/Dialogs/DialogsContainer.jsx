533d9481cef3da76a7e633e80e794e1f9e609c33
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-08-02 17:41:50 +0300

    U69

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Dialogs from './Dialogs';
 import { sendMessageActionCreator, updateNewMessageTextActionCreator } from '../../redux/dialog-reducer';
 import { connect } from "react-redux";
-
+import {Redirect} from "react-router-dom";
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 
 let mapStateToProps = (state) => {
     return {
         dialogs: state.dialogPage.dialogs,
         messages: state.dialogPage.messages,
-        newMessageText: state.dialogPage.newMessageText,
-        isAuth: state.auth.isAuth
+        newMessageText: state.dialogPage.newMessageText
     }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -22,6 +22,8 @@ let mapDispatchToProps = (dispatch) => {
         }
     }
 }
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
+let AuthRedirectComponent = withAuthRedirect(Dialogs);
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(AuthRedirectComponent);
 
 export default DialogsContainer;
