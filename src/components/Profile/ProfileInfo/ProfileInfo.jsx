ad3f40e10c5852e89f50c451d32b420d32ffbc4b
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-08-03 02:19:40 +0300

    U71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 
 const ProfileInfo = (props) => {
@@ -10,12 +11,12 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div className={classes.photo}>
+            {/*<div className={classes.photo}>
                 <img src='https://www.krym4you.com/files/catalog/138/gallery/big//chernoe-more_1435752144.jpg' />
-            </div>
+            </div>*/}
             <div className={classes.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                ava + description
+                <ProfileStatus status={"Hello my friends"}/>
             </div>
 
         </div>
