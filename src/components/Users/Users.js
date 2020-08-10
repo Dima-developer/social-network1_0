5f00ae7475264b6399dfabf2d1b3118ab233146c
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-08-11 02:09:21 +0300

    U81

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -13,6 +13,7 @@ let Users = (props) => {
     return <div>
         <div>
             {pages.map(p => {
+                debugger;
                 return <span className={props.currentPage === p && styles.selectedPage}
                              onClick={() => {
                                  props.onPageChanged(p)
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,6 +9,13 @@ import Users from './Users';
 import Preloader from "../common/Preloader/Preloader";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
+import {
+    getCurrentPage,
+    getFollowingInProgress,
+    getIsFetching,
+    getPageSize, getAllUsers,
+    getTotalUsersCount
+} from "../../redux/users-selectors";
 
 
 
@@ -59,16 +66,16 @@ let mapStateToProps = (state) => {
 
     return {
 
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress
+        users: getAllUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state)
     }
 }
 
 export default compose (
-    connect(mapStateToProps,{follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers})
+    connect(mapStateToProps,{follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers: getUsers})
 )(UsersContainer);
     /*withAuthRedirect*/
