dc5c7fc80b6c50db8f522ad3974adba575f02623
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-08-10 08:46:41 +0300

    U79

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,7 @@ import {required} from "../../ utilities/validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/auth-reducer";
 import {Redirect} from "react-router-dom";
-
+import style from "./../common/FormsControls/FormsControls.module.css";
 
 const LoginForm = (props) => {
     return (
@@ -17,13 +17,16 @@ const LoginForm = (props) => {
             </div>
             <div>
                 <Field placeholder={"Password"}
-                       validate={[required]}
+                       validate={[required]} type={'password'}
                        name={"password"} component={Input} />
             </div>
             <div>
                 <Field type={"checkbox"} name={"rememberMe"} component={Input}
                 validate={[required]} /> remember me
             </div>
+           { props.error && <div className={style.formSummaryError}>
+                <p>{props.error}</p>
+            </div> }
             <div>
                 <button>Login</button>
             </div>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -10,7 +10,7 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 9623;
+            userId = this.props.authorisedUserId;
         }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
@@ -18,7 +18,9 @@ class ProfileContainer extends React.Component {
     render() {
 
         return (
-            <Profile {...this.props} profile={this.props.profile} status={this.props.status}
+            <Profile {...this.props}
+                     profile={this.props.profile}
+                     status={this.props.status}
             updateStatus={this.props.updateStatus}/>
         );
     }
@@ -26,7 +28,9 @@ class ProfileContainer extends React.Component {
 
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    status: state.profilePage.status
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth
 });
 export default compose(
     connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
