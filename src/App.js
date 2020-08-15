32075cb3688286b4202978b7b4185768dbe30bf1
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-08-15 18:13:23 +0300

    U95_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import {BrowserRouter, Route} from 'react-router-dom';
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import {connect} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
-
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-
+import {withSuspense} from "./hoc/withSuspense";
+//import DialogsContainer from './components/Dialogs/DialogsContainer';
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+//import ProfileContainer from "./components/Profile/ProfileContainer";
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {
     componentDidMount() {
         this.props.initializeApp();
     }
+
     render() {
         if (!this.props.initialized) {
             return <Preloader/>
@@ -31,9 +33,9 @@ class App extends React.Component {
                     <Navbar state={this.props.sidebar}/>
                     <div className='app-wrapper-content'>
                         <Route path='/dialogs'
-                               render={() => <DialogsContainer/>}/>
+                               render={withSuspense(DialogsContainer)} />
                         <Route path='/profile/:userId?'
-                               render={() => <ProfileContainer/>}/>
+                               render={withSuspense(ProfileContainer)} />
                         <Route path='/users'
                                render={() => <UsersContainer/>}/>
                         <Route path='/login'
@@ -47,7 +49,8 @@ class App extends React.Component {
                     </div>
                 </div>
             </BrowserRouter>
-        );
+        )
+            ;
     }
 }
 
